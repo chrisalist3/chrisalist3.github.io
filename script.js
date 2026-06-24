2dbc892f27b9e922cd10b38b702a56907ae97eb0
Crystal <chrisalist3@gmail.com>
2026-06-24 21:34:39 +1000

    Add files via upload
    Sample JS and css

---
+++ script.js
@@ -0,0 +1,16 @@
+fetch('./page/sample')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.text();
+  })
+  .then(textData => {
+    secs = textData.split('\n\n');
+    console.log(secs);
+    document.querySelector('main').innerHTML = secs[0];
+    document.getElementById('foot').innerHTML = secs[1];
+  })
+  .catch(error => {
+    document.querySelector('main').innerHTML = '<h2>Error 404</h2><p>This content was not found :(</p>';
+  });
